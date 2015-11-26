410b70d85f4eb0fa40463aa1b9b030de522b0d3e
huangqiong <563985175@qq.com>
2015-11-26 09:46:03 +0800

    first commit

---
+++ static/js/opecode-text.js
@@ -0,0 +1,31 @@
+/**
+ * Created by wangxiaoxin on 15/9/14.
+ */
+
+var OPECODE = new Object();
+
+OPECODE[500] = "内部错误";
+
+OPECODE[800] = "用户错误";
+OPECODE[801] = "没有这样的用户";
+OPECODE[802] = "登陆密码不正确";
+OPECODE[803] = "账户已被锁定，不能登陆";
+OPECODE[804] = "请完整输入数据";
+OPECODE[805] = "请完整输入数据";
+OPECODE[806] = "请完整输入数据";
+OPECODE[807] = "请完整输入数据";
+OPECODE[808] = "请完整输入数据";
+OPECODE[809] = "请完整输入数据";
+OPECODE[810] = "请完整输入数据";
+
+OPECODE[900] = "输入数据有错误";
+OPECODE[901] = "请完整输入数据";
+OPECODE[902] = "参数格式不正确";
+OPECODE[903] = "请完整输入数据";
+OPECODE[904] = "请完整输入数据";
+OPECODE[905] = "请完整输入数据";
+OPECODE[906] = "请完整输入数据";
+OPECODE[907] = "请完整输入数据";
+OPECODE[908] = "请完整输入数据";
+OPECODE[909] = "请完整输入数据";
+OPECODE[910] = "请完整输入数据";
